79e05c6a3a51a1b7747c867a61fae08239de7464
Kunal Vats <1kunalvats9@gmail.com>
2025-07-07 20:36:57 +0530

    done

--- server/src/controllers/message.controller.js
+++ server/src/controllers/message.controller.js
@@ -1,9 +1,6 @@
 import pkg from 'twilio';
 const { Twilio } = pkg; 
-
 import asyncHandler from 'express-async-handler';
-import { NextResponse } from 'next/server'; 
-
 const client = new Twilio(
   process.env.TWILIO_ACCOUNT_SID,
   process.env.TWILIO_AUTH_TOKEN
@@ -19,9 +16,9 @@ export const sendSMS = asyncHandler(async (req, res) => {
         to: `+91${phoneNumber}`, 
       });
 
-      return NextResponse.json({ success: true, sid: message.sid }, { status: 200 });
+      return res.status(200).json({ success: true, sid: message.sid });
     } catch (err) {
       console.error('❌ SMS Error:', err);
-      return NextResponse.json({ success: false, error: err.message || 'An unknown error occurred' }, { status: 500 });
+      return res.status(500).json({ success: false, error: err.message || 'An unknown error occurred' });
     }
   });
